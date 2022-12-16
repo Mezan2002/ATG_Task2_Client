e1607b46cb569b5c0f6971ee07a59cdbe58b2bb0
Mezan2002 <mezanurr009@gmail.com>
2022-12-16 18:01:28 +0600

    Register done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 const Login = () => {
+  const handleLogIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.userName.value;
+    const userPassword = form.userPassword.value;
+    console.log(userName, userPassword);
+  };
   return (
     <div>
       <h2>Login</h2>
-      <input type="text" placeholder="User Name" />
-      <br />
-      <input type="password" placeholder="User Password" />
-      <br />
-      <input type="submit" value="Log In" />
+      <form onSubmit={handleLogIn}>
+        <input type="text" name="userName" placeholder="User Name" />
+        <br />
+        <input
+          type="password"
+          name="userPassword"
+          placeholder="User Password"
+        />
+        <br />
+        <input type="submit" value="Log In" />
+      </form>
     </div>
   );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,16 +1,48 @@
 import React from "react";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const userName = form.userName.value;
+    const userEmail = form.userEmail.value;
+    const userPassword = form.userPassword.value;
+
+    const userData = {
+      userName,
+      userEmail,
+      userPassword,
+    };
+    fetch("http://localhost:5000/registerUser", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          alert("User Created Successfully!");
+        }
+      });
+  };
   return (
     <div>
       <h2>Register</h2>
-      <input type="text" placeholder="User Name" />
-      <br />
-      <input type="email" placeholder="User Email" />
-      <br />
-      <input type="password" placeholder="User Password" />
-      <br />
-      <input type="submit" value="Register" />
+      <form onSubmit={handleRegister}>
+        <input type="text" name="userName" placeholder="User Name" />
+        <br />
+        <input type="email" name="userEmail" placeholder="User Email" />
+        <br />
+        <input
+          type="password"
+          name="userPassword"
+          placeholder="User Password"
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
